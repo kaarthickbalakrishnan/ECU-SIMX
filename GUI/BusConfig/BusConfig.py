1a4fddb703951a176d5358b764be1e3d210385c1
karthikeyan <karthikeyan@KR>
2020-06-22 11:32:18 +0530

    comment lines were added

--- GUI/BusConfig/BusConfig.py
+++ GUI/BusConfig/BusConfig.py
@@ -10,44 +10,44 @@ class BUS_configure(QObject):
     
     def __init__(self):
         QObject.__init__(self)
-        self.bus=" "
+        self.bus=" " #To access "bus" anywhere in the class
 
     
     @pyqtSlot()
     def getBUS_configuration(self):
-            bus_Obj = senderW.findChild(QObject, "bustype_in")
-            ch_Obj = senderW.findChild(QObject, "channel_in")
-            bit_Obj = senderW.findChild(QObject, "bitrate_in")
-            bustype = bus_Obj.property("currentText")
-            channel = ch_Obj.property("currentText")
-            bitrate = int(bit_Obj.property("text"))
-            self.bus = can.interface.Bus(bustype=bustype, channel=channel, bitrate=bitrate)
+            bus_Obj = senderW.findChild(QObject, "bustype_in") #Finding the widget based on its object name
+            ch_Obj = senderW.findChild(QObject, "channel_in")#Finding the widget based on its object name
+            bit_Obj = senderW.findChild(QObject, "bitrate_in")#Finding the widget based on its object name
+            bustype = bus_Obj.property("currentText")#Fetching the details from the identified widget named as bus_obj
+            channel = ch_Obj.property("currentText")#Fetching the details from the identified widget named as ch_obj
+            bitrate = int(bit_Obj.property("text"))#Fetching the details from the identified widget named as bit_obj
+            self.bus = can.interface.Bus(bustype=bustype, channel=channel, bitrate=bitrate)#assigning the respective value to the fields inorder to configure bus
             print("THE BUS HAS BEEN INITIATED")
             print("bustype=",bustype,"\nchannel=",channel,"\nbitrate=",bitrate)
             
     @pyqtSlot() 
-    def send_can(self):  
-            h_dataList = [11,22,33,44,55,66,77,88]  # convert list items to Hex Values
-            C_ID = 0x012       
-            msg = can.Message(arbitration_id=C_ID,data=h_dataList,is_extended_id=False)
+    def send_can(self):  #This function is to check whether the bus is configured or not by sending one simple can message
+            h_dataList = [11,22,33,44,55,66,77,88]  #sample can data
+            C_ID = 0x012 #sample can id       
+            msg = can.Message(arbitration_id=C_ID,data=h_dataList,is_extended_id=False) #initiating can message
             try:
-                self.bus.send(msg)
+                self.bus.send(msg) #sending can message to the bus
                 print("MESSAGE sent \nMSG ID: " + str(hex(C_ID)) + "\tData: " + str(h_dataList))
             except can.CanError:
                 print("Message NOT sent")
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':   #The  main function which allows QML and PYTHON to work collaboratively
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
     
-    bus_configure = BUS_configure()
-    engine.rootContext().setContextProperty("busconfigure", bus_configure)
+    bus_configure = BUS_configure()   #invoking the class by assigning it to a variable
+    engine.rootContext().setContextProperty("busconfigure", bus_configure) ##Rooting the GUI for its response 
     
-    engine.load("busConfigWin.qml")
-    senderW = engine.rootObjects()[0]
+    engine.load("busConfigWin.qml")# To load qml script
+    senderW = engine.rootObjects()[0] #to identify the object names in the GUI
     
     if not engine.rootObjects():
         sys.exit(-1)    
     
-    sys.exit(app.exec_())
+    sys.exit(app.exec_()) #For executing the whole process
--- GUI/BusConfig/busConfigWin.qml
+++ GUI/BusConfig/busConfigWin.qml
@@ -4,23 +4,26 @@ import QtQuick.Controls 2.14
 import QtGraphicalEffects 1.0
 import QtQuick.Extras 1.2
 
-Window {
-    id: sendbutton
-    visible: true
-    width: 370
+Window {              //Creating a plain window
+    id: sendbutton   //ID to identify
+    visible: true    //Enabling visibility
+    width: 370		// Required dimensions were provided
     height: 320
-    color: "#c5cad1"
-    title: qsTr("BUS CONFIGURATION")
+    color: "#c5cad1"  // For background colour 
+    title: qsTr("BUS CONFIGURATION") // Title of the window
 
+		//Adding a new button 
    
     Button {
         id: send_button
         x: 139
         y: 260
-        text: qsTr("CONFIGURE")
-        onClicked: busconfigure.getBUS_configuration();
+        text: qsTr("CONFIGURE")  //  Text to be visible on button
+        onClicked: busconfigure.getBUS_configuration(); // Once the button pressed the python function will be invoked
     }
 
+    // Adding a text to just display
+	
 
     Text {
         id: channel
@@ -28,8 +31,8 @@ Window {
         y: 116
         width: 112
         height: 36
-        text: qsTr("CHANNEL")
-        font.weight: Font.DemiBold
+        text: qsTr("CHANNEL")  // The given text will be displayed 
+        font.weight: Font.DemiBold   //These option are for the look and feel of the text
         verticalAlignment: Text.AlignVCenter
         horizontalAlignment: Text.AlignHCenter
         font.pixelSize: 18
@@ -67,26 +70,29 @@ Window {
         font.pixelSize: 18
     }
 
+   // Adding a text field to get user response as a text
 
     TextField {
-        objectName: "bitrate_in"
+        objectName: "bitrate_in" //Object name to identify and fetch the details given by the user 
         id: bitrate_Input
         x: 172
         y: 186
         width: 125
         height: 36
-        placeholderText: qsTr("bitrate")
+        placeholderText: qsTr("bitrate") //This text will be displayed for the understanding of user
     }
 
+
+	// Adding a drop down list which is called as "Combo Box"
     ComboBox {
-        objectName: "bustype_in"
+        objectName: "bustype_in" //Object name to identify and fetch the details given by the user
         id: bustype1
         x: 172
         y: 47
         width: 125
         height: 36
-        model: ["socketcan","virtual"]
-        onCurrentIndexChanged:
+        model: ["socketcan","virtual"] //The contents of the drop down list is given here
+        onCurrentIndexChanged:    // According to the value of the bustype the respective channels will be listed in another combo box
         {
         	if(currentIndex === 0)
         		channel1.model=["vcan0","vcan1"]
